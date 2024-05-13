46508be5af7d060ad3c62a2896b2ecb22072dcd9
Aleksandr Krainukov <krainukovaleksandr1@gmail.com>
2024-05-13 23:56:47 +0300

    интегрировал отзывы

--- src/components/Otziv.jsx
+++ src/components/Otziv.jsx
@@ -1,41 +1,46 @@
 import Header from "./Header";
 import './Otziv.css'
-export default function LK() {
-  const reviews = [
-    {
-      author: 'Владимир Сорокин',
-      text: 'Водитель приехал вовремя. Грузчики поработали очень аккуратно и быстро. Всем советую эту компанию!'
-    },
-    {author: 'Владимир Сорокин',
-    text: 'Водитель приехал вовремя. Грузчики поработали очень аккуратно и быстро. Всем советую эту компанию!'
-  },
-  {
-    author: 'Владимир Сорокин',
-    text: 'Водитель приехал вовремя. Грузчики поработали очень аккуратно и быстро. Всем советую эту компанию!'
-},
-  {
-    author: 'Владимир Сорокин',
-    text: 'Водитель приехал вовремя. Грузчики поработали очень аккуратно и быстро. Всем советую эту компанию!'
-  },
-  { author: 'Владимир Сорокин',
-    text: 'Водитель приехал вовремя. Грузчики поработали очень аккуратно и быстро. Всем советую эту компанию!'
-}
-  ];
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-  return (
-    <div className="LK">
-      <Header />
-      <main>
-        <h1 className="centered">Отзывы о грузоперевозчиках</h1>
-        <div className="otziv-container">
-          {reviews.map((review, index) => (
-            <div key={index} className="review">
-              <h2>{review.author}</h2>
-              <p>{review.text}</p>
-            </div>
-          ))}
+export default function LK() {
+    const [reviews, setReviews] = useState([]);
+    const [loading, setLoading] = useState(true); // Add loading state
+
+    useEffect(() => {
+        axios.get('https://226a64ab-15e7-4ad5-97b1-fde455fd41ec-00-2ee8zvawkqwj9.sisko.replit.dev/api/feedback',  )
+            .then(response => {
+                setReviews(response.data.feedbacks);
+                setLoading(false);
+
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+
+
+    return (
+        <div className="LK">
+            <Header />
+            <main>
+                <h1 className="centered">Отзывы о грузоперевозчиках</h1>
+                {loading ? (
+                    <div>Loading...</div>
+                ) : reviews.length > 0 ? (
+                    <div className="otziv-container">
+                        {reviews.map((review, index) => (
+                            <div key={index} className="review">
+                                <p >{review.user_id}</p>
+                                <p>{review.message}</p>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div>No reviews available</div>
+                )}
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
