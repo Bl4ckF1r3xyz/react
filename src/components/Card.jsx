1703ebe82b7c744f1c91b215b0aed2f60af1e62e
Bl4ckF1r3xyz <zhele533@gmail.com>
2024-05-16 21:08:13 +0300

    Добавили карточку товара  🛩

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 import Header from "./Header"
 import "./Card.css"
+import React from 'react';
+
 
 
 function MainPage(){
@@ -9,11 +11,12 @@ function MainPage(){
            <div>
       <Header />
     <main>
-    <input className="one" type="text" placeholder="Откуда?" />
+    <div id="content">
+    <input className="zero" type="text" placeholder="Откуда?" />
         <br />
-        <input className="two" type="text" placeholder="Куда?" />
+        <input className="zero-two" type="text" placeholder="Куда?" />
         <br />
-        <input className="three" type="text" placeholder="Укажите примерный вес груза (в кг):" />
+        <input className="zero-three" type="text" placeholder="Укажите примерный вес груза (в кг):" />
         <br />
         <p>Выберите тип груза:</p>
         <br />
@@ -35,7 +38,51 @@ function MainPage(){
             <input type="checkbox" value="email" />
         <label for="contactChoice1">Нужна спец. техника</label>
         <br />
-                <button className="Ras">Рассчитать</button>
+                <button className="Ras">Рассчитать</button> 
+        
+        <div className="truck-info-container">
+            <h1>Вот такие грузовики мы вам подобрали</h1>
+            <div className="truck-card">
+                <img src="your-image-url.jpg" alt="Грузовик" className="truck-image" />
+                <div className="truck-details">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Грузоподъемность</td>
+                                <td>20 т.</td>
+                            </tr>
+                            <tr>
+                                <td>Длина кузова</td>
+                                <td>13.6 м.</td>
+                            </tr>
+                            <tr>
+                                <td>Высота кузова</td>
+                                <td>2.65 м.</td>
+                            </tr>
+                            <tr>
+                                <td>Объём кузова</td>
+                                <td>82 м/куб</td>
+                            </tr>
+                            <tr>
+                                <td>Вид кузова</td>
+                                <td>фура</td>
+                            </tr>
+                            <tr>
+                                <td>Марка грузовика</td>
+                                <td>DONGFENG</td>
+                            </tr>
+                            <tr>
+                                <td>Опыт работы</td>
+                                <td>1 год</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button className="order-button">Заказать</button>
+                </div>
+            </div>
+        </div>
+
+        </div>     
         </main>
       </div>
         </>
