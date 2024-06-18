2795924286d3fd0f4e77fab52c86de72756f1c50
Aleksandr Krainukov <krainukovaleksandr1@gmail.com>
2024-06-19 01:55:23 +0300

    подтянул историю заказов и сделал функционал для заказов

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import Header from "./Header";
 import "./Card.css";
-import {api} from "../utils/api.js";
-
+import { api } from "../utils/api.js";
 
 function MainPage() {
-    const [selectedType, setSelectedType] = useState(''); // State to store the selected radio button value
+    const [selectedType, setSelectedType] = useState('');
     const [responseData, setResponseData] = useState(null);
+    const userInfo = JSON.parse(localStorage.getItem('user'));
     const handleCalculate = () => {
         fetch(`${api}/api/trucks?type=${selectedType}`)
             .then(response => response.json())
             .then(data => {
-                setResponseData(data)
+                setResponseData(data);
                 console.log(data);
             })
             .catch(error => {
@@ -21,7 +21,36 @@ function MainPage() {
 
     const handleRadioChange = (e) => {
         setSelectedType(e.target.value);
-    }
+    };
+
+    const handleOrder = (truckId) => {
+        const orderData = {
+            truk_id: Number(truckId),
+            user_id: Number(userInfo.id),
+            type_id: Number(selectedType),
+            from: document.querySelector('.zero').value,
+            where: document.querySelector('.zero-two').value,
+            weight: Number(document.querySelector('.zero-three').value),
+            status: true,
+            created_at: new Date().toISOString(),
+        };
+
+        fetch(`${api}/api/order`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(orderData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Order placed:', data);
+                // Можно добавить логику обработки успешного заказа здесь, например, обновление состояния или отображение сообщения
+            })
+            .catch(error => {
+                console.error('Error placing order:', error);
+            });
+    };
 
     return (
         <>
@@ -51,46 +80,45 @@ function MainPage() {
                     <div className="truck-info-container">
                         <h1>Вот такие грузовики мы вам подобрали</h1>
                         {responseData && responseData.trucks && responseData.trucks.map((truck) => (
-                            <div className="truck-card">
-                            <img src={truck.img} alt="Грузовик" className="truck-image"/>
-                            <div className="truck-details">
-                                <table>
-                                    <tbody>
-                                    <tr>
-                                        <td>Грузоподъемность</td>
-                                        <td>{truck.load_capacity}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Длина кузова</td>
-                                        <td>{truck.length} м.</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Высота кузова</td>
-                                        <td>{truck.height} м.</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Объём кузова</td>
-                                        <td>{truck.volume} м/куб</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Вид кузова</td>
-                                        <td>фура</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Марка грузовика</td>
-                                        <td>{truck.Brand}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Опыт работы</td>
-                                        <td>{truck.experience} год</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                <button className="order-button">Заказать</button>
+                            <div className="truck-card" key={truck.id}>
+                                <img src={truck.img} alt="Грузовик" className="truck-image" />
+                                <div className="truck-details">
+                                    <table>
+                                        <tbody>
+                                        <tr>
+                                            <td>Грузоподъемность</td>
+                                            <td>{truck.load_capacity} {truck.id}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Длина кузова</td>
+                                            <td>{truck.length} м.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Высота кузова</td>
+                                            <td>{truck.height} м.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Объём кузова</td>
+                                            <td>{truck.volume} м/куб</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Вид кузова</td>
+                                            <td>фура</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Марка грузовика</td>
+                                            <td>{truck.Brand}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Опыт работы</td>
+                                            <td>{truck.experience} год</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <button className="order-button" onClick={() => handleOrder(truck.id)}>Заказать</button>
+                                </div>
                             </div>
-                        </div>
                         ))}
-
                     </div>
                 </main>
             </div>
